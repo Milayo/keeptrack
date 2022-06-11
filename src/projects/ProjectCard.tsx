2d11a01f5492e0043fe4ec80a8545af5ec5912ea
Ijarotimi Tomilayo <moladeijarotimi@gmail.com>
2022-06-11 06:13:04 -0700

    added more files

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Project } from "./Project";
 
 const formatDescription = (description: string) => {
@@ -18,19 +19,21 @@ const ProjectCard = ({ project, onEdit }: ProjectCardProps) => {
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
       <section className="section dark">
-        <h5 className="strong">
-          <strong>{project.name}</strong>
-        </h5>
-        <p>{formatDescription(project.description)}</p>
-        <p>Budget : {project.budget.toLocaleString()}</p>
-        <button
-          className="bordered"
-          onClick={() => {
-            handleEditClick(project);
-          }}
-        >
-          <span className="icon-edit"></span>
-        </button>
+        <Link to={"/projects/" + project.id}>
+          <h5 className="strong">
+            <strong>{project.name}</strong>
+          </h5>
+          <p>{formatDescription(project.description)}</p>
+          <p>Budget : {project.budget.toLocaleString()}</p>
+          <button
+            className="bordered"
+            onClick={() => {
+              handleEditClick(project);
+            }}
+          >
+            <span className="icon-edit"></span>
+          </button>
+        </Link>
       </section>
     </div>
   );
